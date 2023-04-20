a6d0514d853106e071422a89019f789b357018e5
Navnit Anuth <navnit.anuth@bp.com>
2023-04-20 21:09:25 +0100

    Changes

--- Command/Program.cs
+++ Command/Program.cs
@@ -1,10 +1,117 @@
-﻿namespace Command
+﻿using static System.Console;
+
+namespace Command;
+
+public class BankAccount
 {
-    internal class Program
+    private const int OverdraftLimit = -500;
+    private int _balance;
+
+    public void Deposit(int amount)
     {
-        static void Main(string[] args)
+        _balance += amount;
+        WriteLine($"Deposited ${amount}, balance is now {_balance}");
+    }
+
+    public bool Withdraw(int amount)
+    {
+        if (_balance - amount >= OverdraftLimit)
         {
-            Console.WriteLine("Hello, World!");
+            _balance -= amount;
+            WriteLine($"Withdrew ${amount}, balance is now {_balance}");
+            return true;
         }
+
+        return false;
+    }
+
+    public override string ToString()
+    {
+        return $"{nameof(_balance)}: {_balance}";
+    }
+}
+
+public interface ICommand
+{
+    void Call();
+    void Undo();
+}
+
+public class BankAccountCommand : ICommand
+{
+    public enum Action
+    {
+        Deposit,
+        Withdraw
+    }
+
+    private readonly BankAccount _account;
+
+    private readonly Action _action;
+    private readonly int _amount;
+    private bool _succeeded;
+
+    public BankAccountCommand(BankAccount account, Action action, int amount)
+    {
+        _account = account ?? throw new ArgumentNullException(nameof(account));
+        _action = action;
+        _amount = amount;
+    }
+
+    public void Call()
+    {
+        switch (_action)
+        {
+            case Action.Deposit:
+                _account.Deposit(_amount);
+                _succeeded = true;
+                break;
+            case Action.Withdraw:
+                _succeeded = _account.Withdraw(_amount);
+                break;
+            default:
+                throw new ArgumentOutOfRangeException();
+        }
+    }
+
+    public void Undo()
+    {
+        if (!_succeeded) return;
+        switch (_action)
+        {
+            case Action.Deposit:
+                _account.Withdraw(_amount);
+                break;
+            case Action.Withdraw:
+                _account.Deposit(_amount);
+                break;
+            default:
+                throw new ArgumentOutOfRangeException();
+        }
+    }
+}
+
+internal class Demo
+{
+    private static void Main(string[] args)
+    {
+        var ba = new BankAccount();
+        var commands = new List<BankAccountCommand>
+        {
+            new(ba, BankAccountCommand.Action.Deposit, 100),
+            new(ba, BankAccountCommand.Action.Withdraw, 1000)
+        };
+
+        WriteLine(ba);
+
+        foreach (var c in commands)
+            c.Call();
+
+        WriteLine(ba);
+
+        foreach (var c in Enumerable.Reverse(commands))
+            c.Undo();
+
+        WriteLine(ba);
     }
 }
--- Momento/Program.cs
+++ Momento/Program.cs
@@ -1,10 +1,92 @@
-﻿namespace Momento
+﻿using static System.Console;
+
+namespace Momento;
+
+public class Memento
 {
-    internal class Program
+    public Memento(int balance)
     {
-        static void Main(string[] args)
+        Balance = balance;
+    }
+
+    public int Balance { get; }
+}
+
+public class BankAccount
+{
+    private int _balance;
+    private readonly List<Memento?> _changes = new();
+    private int _current;
+
+    public BankAccount(int balance)
+    {
+        this._balance = balance;
+        _changes.Add(new Memento(balance));
+    }
+
+    public Memento? Deposit(int amount)
+    {
+        _balance += amount;
+        var m = new Memento(_balance);
+        _changes.Add(m);
+        ++_current;
+        return m;
+    }
+
+    public void Restore(Memento? m)
+    {
+        if (m != null)
         {
-            Console.WriteLine("Hello, World!");
+            _balance = m.Balance;
+            _changes.Add(m);
+            _current = _changes.Count - 1;
         }
+    }
+
+    public Memento? Undo()
+    {
+        if (_current > 0)
+        {
+            var m = _changes[--_current];
+            _balance = m.Balance;
+            return m;
+        }
+
+        return null;
+    }
+
+    public Memento? Redo()
+    {
+        if (_current + 1 < _changes.Count)
+        {
+            var m = _changes[++_current];
+            _balance = m.Balance;
+            return m;
+        }
+
+        return null;
+    }
+
+    public override string ToString()
+    {
+        return $"{nameof(_balance)}: {_balance}";
+    }
+}
+
+public class Demo
+{
+    private static void Main(string[] args)
+    {
+        var ba = new BankAccount(100);
+        ba.Deposit(50);
+        ba.Deposit(25);
+        WriteLine(ba);
+
+        ba.Undo();
+        WriteLine($"Undo 1: {ba}");
+        ba.Undo();
+        WriteLine($"Undo 2: {ba}");
+        ba.Redo();
+        WriteLine($"Redo 2: {ba}");
     }
 }
